6dc3d54e46f46bfbe7ab26f4f0f5f4855e283281
leijiangping <leijiang21@126.com>
2018-06-11 00:00:40 +0800

    delete

--- ch-recommend-api/src/main/java/cn/chaohi/recommend/common/NEIGHBORHOOD.java
+++
@@ -1,8 +0,0 @@
-/**
- * 构建近邻
- */
-package cn.chaohi.recommend.common;
-
-public enum NEIGHBORHOOD {
-	NEAREST, THRESHOLD
-}
--- ch-recommend-api/src/main/java/cn/chaohi/recommend/common/RECOMMENDER.java
+++
@@ -1,9 +0,0 @@
-
-/**
- * 构建推荐模型
- */
-package cn.chaohi.recommend.common;
-
-public enum RECOMMENDER {
-	USER, ITEM
-}
